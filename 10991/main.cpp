cbc5c01c34b9e2966829ffa2d08f54a22a5d9a74
nayechan <66f@naver.com>
2021-03-13 22:02:50 +0900

    some io problems

---
+++ 10991/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	for(int i=1;i<=n;++i)
+	{
+		for(int j=0;j<n-i;++j)
+			cout << " ";
+		for(int j=0;j<i;++j)
+			cout << "* ";
+		cout << endl;
+	}
+	return 0;
+}
